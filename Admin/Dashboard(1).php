c520f656853853b009d4eb5b09d121e2662ad5a7
Sadanand Jaiswal <sadanandjaiswal92@gmail.com>
2023-05-09 15:05:35 +0530

    first commit

---
+++ Admin/Dashboard(1).php
@@ -0,0 +1,105 @@
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="admin.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+        
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>TNT | Dashboar</title>
+    </head>
+    <body>
+        <div class="body">
+            <div class="sidemenu">
+                
+                <div class="admin_profile">
+                  
+                    <img src="b1.jpg" alt="">
+                    <h5>Admin</h5>
+                  
+                </div>
+                <nav class="nav">
+                    <a href="Dashboard.php" ><li class="li active"><span><i class="fa fa-th-large"></i> </span>Dashboard</li></a>
+                    <a href="RegisteredUser.php"><li class="li"><span><i class="fa fa-users"></i> </span>Registered Usesr</li></a>
+                    <a href="hotels.php"><li class="li"><span><i class="fa fa-hotel"></i> </span>Hotels</li></a>
+                    <a href="placePackage.php"><li class="li"><span><i class="fa fa-birthday-cake"></i> </span>Places (packages)</li></a>
+                    <a href="Booking.php"><li class="li"><span><i class="fa-solid fa-ticket"></i></span> Booking</li></a>
+                    <a href="flight.php"><li class="li"><span><i class="fa-solid fa-plane"></i></span> Flight</li></a>
+                    <a href="train.php"><li class="li"><span><i class="fa-solid fa-train"></i></span> Train</li></a>
+                    <a href="contactUs.php"><li class="li"><span><i class="fa-solid fa-train"></i></span> contact Us</li></a>
+                    <a href="transaction.php"><li class="li"><span><i class="fa-solid fa-money-bill-wave"></i></span> Transaction</li></a>
+                </nav>
+                   
+       
+    
+    
+            </div>
+            <div class="rightframe">
+                <div class="dashboard">
+                    <div class="dashboard_child">
+                        <h3>TOATL ORDER</h3>
+                        <span>
+                            no
+                        </span>
+                          
+                        <p>
+                            TOATL ORDER
+                        </p>
+                    </div>
+                    <div class="dashboard_child">
+                        <h3>NEW ORDER</h3>
+                        <span>
+                            no
+                        </span>
+                          
+                        <p>
+                            NEW ORDER
+                        </p>
+                    </div>
+                    <div class="dashboard_child">
+                        <h3>CONFIRMED ORDER</h3>
+                        <span>
+                        no
+                        </span>
+                          
+                        <p>
+                           CONFIRMED ORDER
+                        </p>
+                    </div>
+                    <!-- <div class="dashboard_child">
+                        <h3>TOTAL CAKE DELIVER</h3>
+                        <span>
+                            5
+                        </span>
+                          
+                        <p>
+                            TOTAL CAKE DELIVER
+                        </p>
+                    </div> -->
+                    <!-- <div class="dashboard_child">
+                        <h3>CANCELLED ORDER</h3>
+                        <span>
+                            5
+                        </span>
+                          
+                        <p>
+                            CANCELLED ORDER
+                        </p>
+                    </div> -->
+                    <div class="dashboard_child">
+                        <h3>TOTAL REGISTERED USER</h3>
+                        <span>
+                        no
+                        </span>
+                        <p>
+                            TOTAL REGISTERED USER
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
---
+++ Admin/booking(1).php
@@ -0,0 +1,95 @@
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="admin.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>TNT | Booking</title>
+    </head>
+    <body>
+        <div class="body">
+            <div class="sidemenu">
+                
+                <div class="admin_profile">
+                  
+                    <img src="b1.jpg" alt="">
+                    <h5>Admin</h5>
+                  
+                </div>
+                <nav class="nav">
+                    <a href="Dashboard.php" ><li class="li"><span><i class="fa fa-th-large"></i> </span>Dashboard</li></a>
+                    <a href="RegisteredUser.php"><li class="li"><span><i class="fa fa-users"></i> </span>Registered Usesr</li></a>
+                    <a href="hotels.php"><li class="li"><span><i class="fa fa-hotel"></i> </span>Hotels</li></a>
+                    <a href="placePackage.php"><li class="li"><span><i class="fa fa-birthday-cake"></i> </span>Places (packages)</li></a>
+                    <a href="Booking.php"><li class="li active"><span><i class="fa-solid fa-ticket"></i></span> Booking</li></a>
+                    <a href="flight.php"><li class="li"><span><i class="fa-solid fa-plane"></i></span> Flight</li></a>
+                    <a href="train.php"><li class="li"><span><i class="fa-solid fa-train"></i></span> Train</li></a>
+                    <a href="contactUs.php"><li class="li"><span><i class="fa-solid fa-train"></i></span> contact Us</li></a>
+                    <a href="transaction.php"><li class="li"><span><i class="fa-solid fa-money-bill-wave"></i></span> Transaction</li></a>
+                </nav>
+    
+    
+            </div>
+            <div class="rightframe">
+                <?php
+
+
+                    $conn2  = mysqli_connect('localhost','root','','travel_website');
+
+                    if(!$conn2){
+                        echo "unable to connect to db";
+                    }
+
+                    echo " <center><h3>Booking</h3></center>";
+                    $sql="SELECT * FROM booking";
+
+                    $res=mysqli_query($conn2,$sql);
+
+                    echo '<center><table border=2 cellspacing=0>
+                    <tr><td>User ID</td><td>Booking ID</td><td>Name</td><td>Email</td><td>Phone</td><td>Address</td><td>From</td><td>To</td><td>Boarding_date</td><td>Arrival Date</td></tr>';
+
+
+
+                    while($result=mysqli_fetch_assoc($res))
+                    {
+                        echo "<style>
+                            .rightframe a{
+                                color:blue;
+                            }
+                        </style>";
+                        echo '<tr>
+                        <td>'.$result['user_id'].'</td>
+                        
+                        <td>'.$result['booking_id'].'</td>
+                        <td>'.$result['name'].'</td>
+                        <td>'.$result['email'].'</td>
+                        <td>'.$result['phone'].'</td>
+                        <td>'.$result['address'].'</td>
+                        <td>'.$result['from'].'</td>
+                        <td>'.$result['to'].'</td>
+                        <td>'.$result['boarding_date'].'</td>
+                        <td>'.$result['arrival_date'].'</td>
+                        
+                        </tr>';
+                    }
+
+                    echo '</table></center><br><br> ';
+
+
+                    // $sql="SELECT * FROM `tbltransacdetail`;";
+
+                    // $result=mysqli_query($conn, $sql);
+
+                    // while($row=mysqli_fetch_assoc($result)){
+                    //     echo ".$row['detail_id']." ".$row['transac_code']." ".$row['date'].";
+                    // }
+
+                ?>
+            </div>
+        </div>
+    </body>
+    </html>
